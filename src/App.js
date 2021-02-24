1988850397b5852f1a7a0d4dabe92b86b58ed732
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 09:13:23 -0800

    Testing works when clicking button + dummy paragraphs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import data from "./data";
+
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
@@ -21,8 +22,22 @@ function App() {
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button type="submit" className="btn">Generate</button>
+        <button type="submit" className="btn">
+          Generate
+        </button>
       </form>
+      <article className="lorem-text">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          rerum quod vero molestias voluptate facilis, eum sapiente. Assumenda
+          ea expedita labore, mollitia officia cupiditate illo?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          rerum quod vero molestias voluptate facilis, eum sapiente. Assumenda
+          ea expedita labore, mollitia officia cupiditate illo?
+        </p>
+      </article>
     </section>
   );
 }
