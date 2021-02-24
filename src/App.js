79bb23ec500c48b8a91ab26bfaad869fbc17422e
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 09:40:37 -0800

    If amount is negative, display only one paragraph

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     let amount = parseInt(count);
+    if(count <= 0) {
+        amount = 1
+    }
+    if(count > 8) {
+        amount = 8
+    }
     setText(data.slice(0,amount));
   };
 
