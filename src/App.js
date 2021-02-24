8835e576fd34b299647b7c7ffcfc76fff174efba
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 09:10:35 -0800

    Form and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Testing');
-    }
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Testing");
+  };
+
   return (
-  <section className="section-center">
+    <section className="section-center">
       <h3>Tired of borem lorem ipsum?</h3>
-      <form className="lorem-form" onSubmit={handleSubmit}></form>
-      <label htmlFor="amount">
-          paragraphs: 
-      </label>
-  </section>
-    )
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">Generate</button>
+      </form>
+    </section>
+  );
 }
 
 export default App;
