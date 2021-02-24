70f43ddb86f58c971ab9f7cbd6c970cb20ac1156
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-23 16:55:20 -0800

    Handle submit function + form label

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ function App() {
   
     const [count, setCount] = useState(0);
     const [text, setText] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Testing');
+    }
   
   return (
   <section className="section-center">
       <h3>Tired of borem lorem ipsum?</h3>
       <form className="lorem-form" onSubmit={handleSubmit}></form>
+      <label htmlFor="amount">
+          paragraphs: 
+      </label>
   </section>
     )
 }
